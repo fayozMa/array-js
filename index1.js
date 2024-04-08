bb8e398163aa5d2bdf2eea1606b580345077a902
Muhammadfayyozbek Ahmadjonov <usmonovbahriddin108@gmail.com>
2024-04-08 23:56:07 +0500

    [fix]

--- index1.js
+++ index1.js
@@ -2,6 +2,6 @@ function reverseArray() {
     let str = "1,2,3,4,5"
     console.log(str);
     let array = str.split(",")
-    console.log(array.reverse());
+    console.log(array);
 }
 reverseArray()
